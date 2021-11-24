81180f821c69db854f09f3f27f40c27b1d6ce19a
Andrey Lyashin <andruiman@gmail.com>
2021-11-24 19:20:46 +0300

    fix

--- src/Contracts/XchgPair/ClassTypes.v
+++ src/Contracts/XchgPair/ClassTypes.v
@@ -12,9 +12,7 @@ Require Import UMLang.LocalClassGenerator.ClassGenerator.
 
 Require Import Project.CommonTypes.
 
-(* Inductive StateInitFields := | StateInit_ι_split_depth | StateInit_ι_special | StateInit_ι_code | StateInit_ι_data | StateInit_ι_library . *)
 Inductive DXchgPairFields := | DXchgPair_ι_flex_addr_ | DXchgPair_ι_tip3_major_root_ | DXchgPair_ι_tip3_minor_root_ | DXchgPair_ι_min_amount_ | DXchgPair_ι_notify_addr_ .
-
 
 Module ClassTypes (xt: XTypesSig) (sm: StateMonadSig) .
 Module Export CommonTypes := Types xt sm.
@@ -24,15 +22,6 @@ Local Open Scope record.
 Local Open Scope program_scope.
 Local Open Scope glist_scope.
 
-
-(* Definition StateInitL : list Type := 
- [ ( ( XMaybe XUInteger ) ) : Type ; 
- ( ( XMaybe TickTockLRecord ) ) : Type ; 
- ( ( XMaybe XCell ) ) : Type ; 
- ( ( XMaybe XCell ) ) : Type ; 
- ( ( XMaybe XCell ) ) : Type ] .
-Elpi GeneratePruvendoRecord StateInitL StateInitFields . 
- *)
 Definition DXchgPairL : list Type := 
  [ ( XAddress ) : Type ; 
  ( XAddress ) : Type ; 
